60b7249532a738a7589054f5b2d1ebd242d31f69
Banggi Bima <databasebbe@gmail.com>
2019-04-06 21:57:40 +0700

    Initial commit

---
+++ app/src/main/java/id/banggibima/mobi/SplashActivity.kt
@@ -0,0 +1,12 @@
+package id.banggibima.mobi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
